ffa11d18d0b2e3124132209f6ff73e78dcc57ec9
KamileSeidu <kseidu005@st.eg.edu.gh>
2021-11-01 09:16:33 +0000

    New update

--- Navigator/CheckoutNavigator.js
+++ Navigator/CheckoutNavigator.js
@@ -11,7 +11,7 @@ const Tab = createMaterialTopTabNavigator();
 function MyTabs() {
     return (
       <Tab.Navigator>
-        <Tab.Screen name="Shipping" component={Checkout} />
+        {/* <Tab.Screen name="Shipping" component={Checkout} /> */}
         <Tab.Screen name="Payment" component={Payment} />
         <Tab.Screen name="Confirm" component={Confirm} />
       </Tab.Navigator>
--- screens/Checkout.js
+++ screens/Checkout.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Text, View} from 'react-native';
 
 function Checkout(props) {
     return (
-         <View>
-             <Text>
-                 Checkout
-             </Text>
-         </View>
+       <View>
+           <Text>Checkout</Text>
+       </View>
     );
 }
 
